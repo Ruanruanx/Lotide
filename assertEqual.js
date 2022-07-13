9070d37dbeca7c94bd4a60b3364687d449ce852b
Ruanruanx <zxin7@uwo.ca>
2022-07-13 19:55:51 +0000

    new Print style

--- assertEqual.js
+++ assertEqual.js
@@ -1,21 +1,21 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-let len=actual.length;
-let result='';
-for(let i=0; i<len;i++){
-  if(actual[i]!==expected[i]){
-    result='Failed';
-    break;
+const assertEqual = function (actual, expected) {
+  let len = actual.length;
+  let result = '';
+  for (let i = 0; i < len; i++) {
+    if (actual[i] !== expected[i]) {
+      result = 'Failed';
+      break;
+    }
   }
-}
-if(len!==expected.length){
-  result='Failed';
-}
-if( result==='Failed'){
-  console.log("Assertion Failed:"+actual +"!=="+expected)
-}else{
-  console.log("Assertion Passed:"+actual +"==="+expected)
-}
+  if (len !== expected.length) {
+    result = 'Failed';
+  }
+  if (result === 'Failed') {
+    console.log(`Assertion Failed: ${actual}  !== ${expected}`)
+  } else {
+    console.log(`Assertion Passed: ${actual} === ${expected}`)
+  }
 
 
 };
