53c2e65b43fcb5415cfa1ceae042c7278f8b83ea
Ruanruanx <zxin7@uwo.ca>
2022-07-17 17:12:44 +0000

    added assertEqual,assertArraysEqual

--- flatten.js
+++ flatten.js
@@ -1,3 +1,48 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  actual = actual.toString();
+  expected = expected.toString();
+  let len = actual.length;
+  let result = 'Passsed';
+  for (let i = 0; i < len; i++) {
+    if (actual[i] !== expected[i]) {
+      result = 'Failed';
+      break;
+    }
+  }
+  if (len !== expected.length) {
+    result = 'Failed';
+  }
+  if (result === 'Failed') {
+    console.log(`Assertion Failed: ${actual}  !== ${expected}`);
+  } else {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  }
+  return result;
+};
+
+const eqArrays = function(arr1, arr2) {
+  let len1 = arr1.length;
+  let len2 = arr2.length;
+  let result = true;
+  if (len1 !== len2) {
+    result = false;
+  } else {
+    for (let i = 0; i < len1; i++) {
+      if (assertEqual(arr1[i], arr2[i]) === 'Failed') {
+        result = false;
+        break;
+      }
+    }
+  }
+  return result;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  console.log(assertEqual(eqArrays(arr1, arr2), true));
+};
+
+
 const flatten = function(input) {
   let arr = [];
   for (let ele of input) {
@@ -9,7 +54,9 @@ const flatten = function(input) {
       }
     }
   }
-  console.log(arr);
+  //console.log(arr);
+  return arr;
 };
 
-flatten([1, 2, [3, 4], 5, [6]]); // => [1, 2, 3, 4, 5, 6]
+let arr = flatten([1, 2, [3, 4], 5, [6]]);
+assertArraysEqual(arr, [1, 2, 3, 4, 5, 6]); // => [1, 2, 3, 4, 5, 6]
