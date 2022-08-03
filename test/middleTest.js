a5e7773b10996536c60c083ca1a61904cafa1fd3
Ruanruanx <zxin7@uwo.ca>
2022-08-03 18:01:09 +0000

    mocha & chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,22 @@
+// const middle = require('../middle');
+
+// const assertArraysEqual = require('../assertArraysEqual');
+
+// //TEST CODE
+// let arr1 = middle([1, 2, 3, 5,9, 6]); // => [2, 3]
+// assertArraysEqual(arr1, [3,5]);
+
+// let arr2 = middle([1, 2, 3, 5,9]);
+// assertArraysEqual(arr2,[3]);
+
 const middle = require('../middle');
+const assert = require("chai").assert;
 
-const assertArraysEqual = require('../assertArraysEqual');
-
-//TEST CODE
-let arr1 = middle([1, 2, 3, 5,9, 6]); // => [2, 3]
-assertArraysEqual(arr1, [3,5]);
-
-let arr2 = middle([1, 2, 3, 5,9]);
-assertArraysEqual(arr2,[3]);
+describe("#middleTest", () => {
+  it("should return [3,5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 5, 9, 6]), [3, 5]);
+  });
+  it("should return [3]", () => {
+    assert.deepEqual(middle([1, 2, 3, 5, 9,]), [3]);
+  });
+});
