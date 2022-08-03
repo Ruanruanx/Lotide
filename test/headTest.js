0bcda1c0cea223bdecfc9675d1a91b18456dcd45
Ruanruanx <zxin7@uwo.ca>
2022-08-03 17:49:17 +0000

    mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,25 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+// const head = require('../head');
+
+
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+const assert = require('chai').assert;
 const head = require('../head');
 
+describe("#head", () => {
+  it("returns 1 for [1,2,3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  it("returns '5' for '5'", () => {
+    assert.strictEqual(head(['5']),'5');
+  });
+
+  it("should return Hello",()=>{
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+}
+
+);
