7f0624aa3d4ba28dab6dd4e24c69f90d0b7bc51e
Ruanruanx <zxin7@uwo.ca>
2022-08-05 17:51:20 +0000

    assertObject Equal

--- countLetters.js
+++ countLetters.js
@@ -1,32 +1,4 @@
-
-const assertEqual = function(actual, expected) {
-  let result = '';
-  if (actual === undefined) {
-    if (expected !== undefined) {
-      result = 'Failed';
-    }
-  } else {
-    actual = actual.toString();
-    expected = expected.toString();
-    let len = actual.length;
-
-    for (let i = 0; i < len; i++) {
-      if (actual[i] !== expected[i]) {
-        result = 'Failed';
-        break;
-      }
-    }
-    if (len !== expected.length) {
-      result = 'Failed';
-    }
-  }
-  if (result === 'Failed') {
-    console.log(`Assertion Failed: ${actual}  !== ${expected}`);
-  } else {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 const countLetters = function(string) {
   //iterate the string, convert it to array
