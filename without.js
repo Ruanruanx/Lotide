6f447ebfb0313ca6ecebfc4349302972b35f09ad
Ruanruanx <zxin7@uwo.ca>
2022-08-05 19:08:49 +0000

    modify without

--- without.js
+++ without.js
@@ -1,50 +1,10 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  actual = actual.toString();
-  expected = expected.toString();
-  let len = actual.length;
-  let result = 'Passsed';
-  for (let i = 0; i < len; i++) {
-    if (actual[i] !== expected[i]) {
-      result = 'Failed';
-      break;
-    }
-  }
-  if (len !== expected.length) {
-    result = 'Failed';
-  }
-  if (result === 'Failed') {
-    console.log(`Assertion Failed: ${actual}  !== ${expected}`);
-  } else {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  }
-  return result;
-};
-
-const eqArrays = function(arr1, arr2) {
-  let len1 = arr1.length;
-  let len2 = arr2.length;
-  let result = true;
-  if (len1 !== len2) {
-    result = false;
-  } else {
-    for (let i = 0; i < len1; i++) {
-      if (assertEqual(arr1[i], arr2[i]) === 'Failed') {
-        result = false;
-        break;
-      }
-    }
-  }
-  return result;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  console.log(assertEqual(eqArrays(arr1, arr2), true));
-};
 
 //iterate item in arr
 //iterate item in iTR array
 //push item to new array if 2nd item equals ele not in arr
+const assertEqual = require('./assertEqual');
+
 const without = function(arr, itemsToRemove) {
   let newArr = [];
   let arrLen = arr.length;
@@ -56,13 +16,8 @@ const without = function(arr, itemsToRemove) {
       }
     }
   }
-  console.log(newArr);
+  //console.log(newArr);
   return newArr;
 };
-
-// const words = ["hello", "world", "lighthouse"];
-// without(words, ["lighthouse"]); // no need to capture return value for this test case
-// // Make sure the original array was not altered by the without function
-// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
 module.exports = without;
