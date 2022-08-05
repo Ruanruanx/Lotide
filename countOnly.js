db58d055b2bf04b51d4221c4be251acb01504f4e
Ruanruanx <zxin7@uwo.ca>
2022-08-05 19:21:16 +0000

    remove assert

--- countOnly.js
+++ countOnly.js
@@ -1,36 +1,6 @@
 //define extreme case
 //define general case, then add condition to define specific case
 
-// const assertEqual = require('./assertEqual');
-// const assertEqual = function(actual, expected) {
-//   let result = '';
-//   if (actual === undefined) {
-//     if (expected !== undefined) {
-//       result = 'Failed';
-//     }
-//   } else {
-//     actual = actual.toString();
-//     expected = expected.toString();
-//     let len = actual.length;
-
-//     for (let i = 0; i < len; i++) {
-//       if (actual[i] !== expected[i]) {
-//         result = 'Failed';
-//         break;
-//       }
-//     }
-//     if (len !== expected.length) {
-//       result = 'Failed';
-//     }
-//   }
-//   if (result === 'Failed') {
-//     console.log(`Assertion Failed: ${actual}  !== ${expected}`);
-//   } else {
-//     console.log(`Assertion Passed: ${actual} === ${expected}`);
-//   }
-
-// };
-
 const countOnly = function(allItems, itemsToCount) {
   const result = {};
   for (const item of allItems) {
