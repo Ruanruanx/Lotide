dc18838b67f58179bb906e05abde79c027e4d160
Ruanruanx <zxin7@uwo.ca>
2022-08-05 20:51:11 +0000

    add recursive test

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,5 +1,15 @@
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
 
 // TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 4,3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 4, 3]), true);
+describe("eqArray", () => {
+  it("test 1", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 4, 3]), false);
+  });
+  it("test 2", () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+  });
+});
+
