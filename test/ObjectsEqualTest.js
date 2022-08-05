2507cdb5084d5f1cd4dedb013cbabfd2f7bf3cc9
Ruanruanx <zxin7@uwo.ca>
2022-08-05 21:02:14 +0000

    use mocha test

--- test/ObjectsEqualTest.js
+++ test/ObjectsEqualTest.js
@@ -1,8 +1,18 @@
 const assertObjectsEqual = require('../assertObjectsEqual');
+const assert = require('chai').assert;
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 const cd2 = { c: "1", d: ["2", 3, 4] };
 
-assertObjectsEqual(cd, dc);
-assertObjectsEqual(cd, cd2);
+let result1 = assertObjectsEqual(cd, dc);
+let result2 = assertObjectsEqual(cd, cd2);
+
+describe("#objects", () => {
+  it("test 1", () => {
+    assert.strictEqual(result1, true);
+  });
+  it("test 2", () => {
+    assert.strictEqual(result2, false);
+  });
+});
--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,3 +1,8 @@
 const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-assertArraysEqual([1, 2, 3], [1, 2, 4]);
+describe('#assertArraysEqual', () => {
+  it("test 1", () => {
+    assert.deepEqual(assertArraysEqual([1, 2, 3], [1, 2, 4]), true);
+  });
+});
