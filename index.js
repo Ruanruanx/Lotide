13572d6c712d242af996b8a72ccb21600cfc8b0e
Ruanruanx <zxin7@uwo.ca>
2022-08-05 20:36:17 +0000

    add index

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const findKeyByValue = require('./findKeyByValue');
 const flatten = require('./flatten');
 const letterPositions = require('./letterPositions');
 const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 module.exports = {
   head,
@@ -29,5 +31,7 @@ module.exports = {
   findKeyByValue,
   flatten,
   letterPositions,
-  map
+  map,
+  without,
+  takeUntil
 };
