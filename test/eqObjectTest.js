3a0c05013754ffb6f4d688032aaa4120312451a1
Ruanruanx <zxin7@uwo.ca>
2022-08-05 21:09:15 +0000

    object mocha test

--- test/eqObjectTest.js
+++ test/eqObjectTest.js
@@ -1,11 +1,19 @@
 const eqObjects = require('../eqObjects');
-
+const assert = require('chai').assert;
 //array test
 console.log('array test');
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-eqObjects(cd, dc); // => true
+let result1 = eqObjects(cd, dc); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log('----');
-eqObjects(cd, cd2); // => false
+let result2 = eqObjects(cd, cd2); // => false
+
+describe("#eqObject",()=>{
+  it("test 1",()=>{
+    assert.strictEqual(result1, true);
+  });
+  it("test 2",()=>{
+    assert.strictEqual(result2, false);
+  });
+});
