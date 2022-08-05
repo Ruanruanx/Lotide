6045d47785a48d8ef82d1516cf27a087cadc4f66
Ruanruanx <zxin7@uwo.ca>
2022-08-05 17:29:53 +0000

    modify

--- without.js
+++ without.js
@@ -60,9 +60,9 @@ const without = function(arr, itemsToRemove) {
   return newArr;
 };
 
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+// const words = ["hello", "world", "lighthouse"];
+// without(words, ["lighthouse"]); // no need to capture return value for this test case
+// // Make sure the original array was not altered by the without function
+// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
-
+module.exports = without;
