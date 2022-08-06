e6f3869b535aa7d4c5a48252dfd2d2bb1d2ba69f
Ruanruanx <zxin7@uwo.ca>
2022-08-06 18:01:15 +0000

    modify output

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,7 +3,9 @@ const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
-  console.log(assertEqual(eqArrays(arr1, arr2), true));
+  if (assertEqual(eqArrays(arr1, arr2))) {
+    console.log(`Assertion passed ${arr1} = ${arr2}`);
+  }
 };
 
 module.exports = assertArraysEqual;
