3e43b99453d99e66c6d13519052eeb994af29b46
Ruanruanx <zxin7@uwo.ca>
2022-08-07 19:43:28 +0000

    add return

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -6,8 +6,10 @@ const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
   if (eqObjects(actual, expected)) {
     console.log(`Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+    return true;
   } else {
     console.log(`Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+    return false;
   }
 };
 
