f48b7b4f07fa4ceed6dc865c9023b0d6abf62bd3
Ruanruanx <zxin7@uwo.ca>
2022-08-07 19:24:30 +0000

    replace let by const

--- eqObjects.js
+++ eqObjects.js
@@ -1,23 +1,27 @@
 const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
 
 const eqObjects = function(object1, object2) {
   //iterate key in obj1, compare the values by key
   const count1 = Object.keys(object1).length;
-  const  count2 = Object.keys(object2).length;
+  const count2 = Object.keys(object2).length;
   if (count1 !== count2) {
     return false;
   }
   let result = true;
-  for (let key in object1) {
+  for (const key in object1) {
+    //use eqArrays if the elements are arrays
     if (Array.isArray(object1[key])) {
       let arrResult = eqArrays(object1[key], object2[key]);
       if (!arrResult) {
         result = false;
       }
+      //recursive
     } else if (typeof (object1[key]) === 'object') {
       result = eqObjects(object1[key], object2[key]);
     } else {
-      if ((object1[key] !== object2[key])) {
+      //if values are string, boolean or number
+      if (!assertEqual(object1[key], object2[key])) {
         result = false;
       }
     }
@@ -25,6 +29,5 @@ const eqObjects = function(object1, object2) {
   }
   return result;
 };
-console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }));
 
 module.exports = eqObjects;
