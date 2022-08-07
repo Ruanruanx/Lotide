4fff9892c59f96c3c3562fa1bbedff19a76baf86
Ruanruanx <zxin7@uwo.ca>
2022-08-07 18:43:26 +0000

    modify argument number

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -3,6 +3,6 @@ const assert = require('chai').assert;
 
 describe('#assertArraysEqual', () => {
   it("test 1", () => {
-    assert.deepEqual(assertArraysEqual([1, 2, 3], [1, 2, 4]), true);
+    assert.strictEqual(assertArraysEqual([1, 2, 3], [1, 2, 4]), true);
   });
 });
