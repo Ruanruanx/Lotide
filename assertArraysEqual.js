0a7143ef64179036e1514e60d89ef38046743fe3
Ruanruanx <zxin7@uwo.ca>
2022-08-07 18:43:08 +0000

    modify argument number

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,8 +3,11 @@ const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
-  if (assertEqual(eqArrays(arr1, arr2))) {
+  console.log(assertEqual(false,true));
+  if (assertEqual(eqArrays(arr1, arr2),true)) {
     console.log(`Assertion passed ${arr1} = ${arr2}`);
+  } else {
+    console.log(`Assertion failed ${arr1} != ${arr2}`);
   }
 };
 
