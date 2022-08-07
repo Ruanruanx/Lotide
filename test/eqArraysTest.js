b51315d0f5fc5952a96ba8eccbed2a1de454e09f
Ruanruanx <zxin7@uwo.ca>
2022-08-07 18:24:33 +0000

    add test 4&5

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -5,13 +5,19 @@ const assert = require('chai').assert;
 
 describe("eqArray", () => {
   it("test 1", () => {
-    assert.strictEqual(eqArrays([1, 2, 3], [1, 4, 3]), false);
+    assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
   });
   it("test 2", () => {
     assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
   });
-  it("test 3",() =>{
+  it("test 3", () => {
     assert.strictEqual(eqArrays(["1", "2", 3], ["1", "2", '3']), false);
+  });
+  it("test 4", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it("test 5",()=>{
+    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", '3']), true);
   });
 });
 
