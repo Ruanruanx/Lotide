ea974bc1334b163da2f483681cbab7c21d3c39b4
Ruanruanx <zxin7@uwo.ca>
2022-08-07 19:03:04 +0000

    add return value

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,11 +3,13 @@ const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
-  console.log(assertEqual(false,true));
+  //console.log(assertEqual('Failed',true));
   if (assertEqual(eqArrays(arr1, arr2),true)) {
     console.log(`Assertion passed ${arr1} = ${arr2}`);
+    return true;
   } else {
     console.log(`Assertion failed ${arr1} != ${arr2}`);
+    return false;
   }
 };
 
